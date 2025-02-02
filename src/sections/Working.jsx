baa366e9f2eb990df43d7bb656ea59943227e417
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2025-02-02 15:44:35 -0800

    style: base working section

--- src/sections/Working.jsx
+++ src/sections/Working.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { planning } from '../export'
 import { motion } from 'framer-motion';
 import { slideUpVariants, zoomInVariants } from './animation';
@@ -11,9 +10,9 @@ const Working = () => {
         whileInView="visible"
         variants={slideUpVariants}
         className='lg:w-[80%] w-[90%] m-auto py-[60px] flex flex-col justify-between items-center gap-[20px]'>
-        <motion.h1 variants={slideUpVariants} className=' text-yellow-500 text-2xl'>STEP BY STEP</motion.h1>
+        <motion.h1 variants={slideUpVariants} className=' text-secondary text-2xl'>STEP BY STEP</motion.h1>
         <motion.h1 variants={slideUpVariants} className='text-black uppercase text-[40px] font-bold text-center'>HOW IT'S WORKING</motion.h1>
-        <motion.div variants={slideUpVariants} className='w-[120px] h-[6px] bg-yellow-500'></motion.div>
+        <motion.div variants={slideUpVariants} className='w-[120px] h-[6px] bg-primary'></motion.div>
         <motion.div
           initial="hidden"
           whileInView="visible"
@@ -21,9 +20,9 @@ const Working = () => {
           className='w-full grid lg:grid-cols-4 grid-cols-1 justify-center items-start gap-[20px] mt-[30px]'>
           {
             planning.map((item, index) => (
-              <div key={index} className='flex flex-col justify-center items-center gap-5 border-2 border-yellow-500 rounded-md p-6'>
+              <div key={index} className='flex flex-col justify-center items-center gap-5 border-2 border-primary rounded-md p-6'>
                 <div>
-                  <item.icon className='size-[80px] bg-yellow-500 hover:bg-black hover:fill-white p-4 rounded-full' />
+                  <item.icon className='size-[80px] bg-primary fill-white p-4 rounded-full' />
                 </div>
                 <h1 className='text-2xl font-bold uppercase'>{item.title}</h1>
                 <p className='text-[20px] text-center text-gray-600'>{item.about}</p>
